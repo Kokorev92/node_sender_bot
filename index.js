220c9a3d443027ede654a88f7d3709c21159b21a
Yaroslav <kokorevys@nppstrela.ru>
2023-04-24 16:23:53 +0300

    [wip]: прикручиваю базу данных

--- index.js
+++ index.js
@@ -1,22 +1,65 @@
 const TelegramBot = require('node-telegram-bot-api')
 const express = require('express')
 const settings = require('./settings.json')
+const Sequelize = require('sequelize')
 
-const bot = new TelegramBot(settings.token, { polling: true })
+// const bot = new TelegramBot(settings.token, { polling: true })
 var app = express()
 
-bot.onText(/\/start/, (msg) => {
-    const id = msg.chat.id
-    bot.sendMessage(id, 'Привет')
-    // TODO: add user to database
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: 'bot.db'
 })
 
 
-bot.onText(/\/stop/, (msg) => {
-    const id = msg.chat.id
-    bot.sendMessage(id, 'До свидания!')
-    // TODO: delete user from database 
-})
+
+async function get_db_data() {
+    const users_db = sequelize.define("users", {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true
+        },
+        t_login: {
+            type: Sequelize.STRING
+        },
+        pass: {
+            type: Sequelize.STRING
+        }
+    },
+        {
+            freezeTableName: true,
+            timestamps: false
+        }
+    )
+
+    // const usr = await users_db.create({
+    //     id: 111111,
+    //     t_login: 'pidr',
+    //     pass: 'govnoed'
+    // })
+
+    const users_list = await users_db.findAll()
+
+    sequelize.close()
+
+    users_list.forEach(elem => {
+        console.log(elem.id + ' ' + elem.t_login + ' ' + elem.pass)
+    })
+}
+
+get_db_data()
+// bot.onText(/\/start/, (msg) => {
+//     const id = msg.chat.id
+//     bot.sendMessage(id, 'Привет ' + msg.chat.username)
+//     // TODO: add user to database
+// })
+
+
+// bot.onText(/\/stop/, (msg) => {
+//     const id = msg.chat.id
+//     bot.sendMessage(id, 'До свидания!')
+//     // TODO: delete user from database 
+// })
 
 app.use(express.static(__dirname + "/public"))
 app.use(express.json())
@@ -26,7 +69,7 @@ app.listen(8000)
 app.post('/', (req, res) => {
     console.log(req.body)
     if (req.body) {
-        bot.sendMessage(settings.id, req.body.data)
+        // bot.sendMessage(settings.id, req.body.data)
     }
     res.send()
 })
