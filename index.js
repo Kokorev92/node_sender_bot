55ae0e9c4c94f5008372f4a0eed2927e871d887e
Kokorev92 <kokorev.yaroslav@gmail.com>
2023-04-25 00:44:13 +0300

    [wip]: переписал все вызовы и обращения к БД

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ const users_db = sequelize.define("users", {
         primaryKey: true
     },
     t_login: {
-        type: Sequelize.STRING
+        type: Sequelize.TEXT
     },
     pass: {
-        type: Sequelize.STRING
+        type: Sequelize.TEXT
     }
 },
     {
@@ -29,28 +29,11 @@ const users_db = sequelize.define("users", {
     }
 )
 
-async function get_user(pass) {
-    let user = await users_db.findOne({ where: { pass: `${pass}` } })
-
-    return user
-}
-
-async function delete_user(id) {
-    await users_db.destroy({
-        where: {
-            id: id
-        }
-    })
-}
-
-async function add_user(id, login, password) {
-    await users_db.create({ id: id, t_login: login, pass: password })
-        .catch(err => {
-            if (err.name == 'SequelizeUniqueConstraintError') {
-                console.log('User already exists')
-            }
-        })
-}
+sequelize.sync().then(result => {
+    console.log(result)
+}).catch(err => {
+    console.log(err)
+})
 
 bot.onText(/\/start/, (msg) => {
     const id = msg.chat.id
@@ -59,16 +42,29 @@ bot.onText(/\/start/, (msg) => {
 })
 
 bot.onText(/\/reg (.+)/, (msg, match) => {
-    add_user(msg.chat.id, msg.chat.username, match[1]).then(res => {
-        bot.sendMessage(msg.chat.id, `Поздравляем, ${msg.chat.username}! Вы успешно зарегестрированы и можете пересылать сообщения через http://kokorev-test:8020/`)
+    users_db.create({ id: msg.chat.id, t_login: msg.chat.username, pass: match[1] }).then(res => {
+        bot.sendMessage(msg.chat.id,
+            `Поздравляем, ${msg.chat.username}! Вы успешно зарегестрированы и можете пересылать сообщения через http://kokorev-test:8020/`)
     })
+        .catch(err => {
+            if (err.name === 'SequelizeUniqueConstraintError') {
+                bot.sendMessage(msg.chat.id, `Вы уже зарегестрированы, ${msg.chat.username}!`)
+            }
+        })
 })
 
-
 bot.onText(/\/stop/, (msg) => {
-    const id = msg.chat.id
-    bot.sendMessage(id, 'До свидания!')
-    delete_user(id)
+    users_db.findOne({ where: { id: msg.chat.id } }).then((user) => {
+        users_db.destroy({
+            where: {
+                id: user.id
+            }
+        }).then(res => {
+            bot.sendMessage(user.id, `До свидания, ${msg.chat.username}!`)
+        })
+    }).catch(err => {
+        bot.sendMessage(msg.chat.id, `Вы не зарегистрированы!`)
+    })
 })
 
 app.use(express.static(__dirname + "/public"))
@@ -79,10 +75,14 @@ app.listen(8000)
 app.post('/', (req, res) => {
     console.log(req.body)
     if (req.body) {
-        get_user(req.body.password).then((user) => {
+        users_db.findOne({ where: { pass: req.body.password } }).then((user) => {
             bot.sendMessage(user.id, req.body.data)
+            let response = { 'status': 'OK' }
+            res.send(JSON.stringify(response))
+        }).catch(err => {
+            let response = { 'status': 'AUTH_ERROR' }
+            res.send(JSON.stringify(response))
         })
-            .catch(err => { })
     }
-    res.send()
+
 })
